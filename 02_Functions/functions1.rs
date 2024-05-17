9a73bb81350bea61144d9c351ebd66c6fc516c3c
Pranav Krishna <138677379+PranavKrishna6939@users.noreply.github.com>
2024-05-18 04:03:06 +0530

    Add files via upload

---
+++ 02_Functions/functions1.rs
@@ -0,0 +1,14 @@
+// functions1.rs
+//
+// Execute `rustlings hint functions1` or use the `hint` watch subcommand for a
+// hint.
+
+// I AM NOT DONE
+
+fn call_me() {
+	println!("Hello World!")
+}
+
+fn main() {
+    call_me();
+}
