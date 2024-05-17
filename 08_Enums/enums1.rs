d570a11c3e68f44a126eafc4ba1ffcd82e79377f
Pranav Krishna <138677379+PranavKrishna6939@users.noreply.github.com>
2024-05-18 04:06:30 +0530

    Add files via upload

---
+++ 08_Enums/enums1.rs
@@ -0,0 +1,21 @@
+// enums1.rs
+//
+// No hints this time! ;)
+
+// I AM NOT DONE
+
+#[derive(Debug)]
+enum Message {
+    // TODO: define a few types of messages as used below
+    Quit,
+    Echo,
+    Move,
+    ChangeColor,
+}
+
+fn main() {
+    println!("{:?}", Message::Quit);
+    println!("{:?}", Message::Echo);
+    println!("{:?}", Message::Move);
+    println!("{:?}", Message::ChangeColor);
+}
